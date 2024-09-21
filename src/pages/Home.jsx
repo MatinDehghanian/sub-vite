ea6eb5e85138e6186404604d46a56077d5d225da
Matin Dehghanian <matindehghanian98@gmail.com>
2024-09-21 22:26:19 +0330

    Refactor ServiceInfo component, update totalTraffic handling, and add react-helmet dependency

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import { Helmet } from "react-helmet";
 import ServiceInfo from "../components/ServiceInfo";
 import Header from "../layouts/Header";
 import Client from "../components/Client";
@@ -6,22 +7,39 @@ import { useEffect, useState } from "react";
 import GetInfoRequest from "../utils/GetInfoRequest";
 
 const Home = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null); // Start with null
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    GetInfoRequest.getInfo().then((res) => setData(res?.data));
+    GetInfoRequest.getInfo()
+      .then((res) => {
+        setData(res?.data);
+      })
+      .finally(() => setLoading(false));
   }, []);
-
 
   return (
     <>
       <Container>
-        {data && (
-          <>
-            <Header />
-            <ServiceInfo data={data} />
-            <Client data={data} />
-          </>
+        <Helmet>
+          <title>
+            {data?.username ? `${data.username} Sub Info` : "Sub Info"}
+          </title>
+          <meta
+            name="description"
+            content="Powered by https://github.com/MatinDehghanian"
+          />
+        </Helmet>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          data && (
+            <>
+              <Header />
+              <ServiceInfo data={data} />
+              <Client data={data} />
+            </>
+          )
         )}
       </Container>
     </>
